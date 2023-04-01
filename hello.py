d8bff5b4100515829fc37da126b95e864fc0ea62
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-04-01 14:56:23 +0300

    feat: strings

--- hello.py
+++ hello.py
@@ -85,26 +85,71 @@
 
 # Numbers
 
-x = 1
-y = 2.8  
-z = 1j
+# x = 1
+# y = 2.8  
+# z = 1j
 
-print(type(x))
-print(type(y))
-print(type(z))
+# print(type(x))
+# print(type(y))
+# print(type(z))
 
-a = float(x)
-b = int(y)
-c = complex(x)
+# a = float(x)
+# b = int(y)
+# c = complex(x)
 
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# import random
+
+# print(random.randrange(1, 10))
+
+# Strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
 print(a)
-print(b)
-print(c)
+print(a[1])
 
-print(type(a))
-print(type(b))
-print(type(c))
+for x in "banana":
+  print(x)
 
-import random
+print(len(a))
 
-print(random.randrange(1, 10))
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+b = "Hello World!"
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+print(b[-5: -2])
+
+print(b.upper())
+print(b.lower())
+
+a = " Hello, World! "
+print(a.strip())
+print(a.replace("H", "J"))
+print(a.split(","))
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
