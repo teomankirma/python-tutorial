f108c0568b4adbcdfbe1dfc632a18f31926c870f
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-04-01 14:14:47 +0300

    feat: python variables

--- hello.py
+++ hello.py
@@ -1,4 +1,27 @@
+# Hello World!
 # print("Hello World!")
 
-if (5 > 2):
-    print("Five is greater than two")
+# If statement
+# if (5 > 2):
+#     print("Five is greater than two")
+
+# Variables
+
+x = 5
+y = "teo"
+print(x)
+print(y)
+
+x = 4
+x = "seker"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
