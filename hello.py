9816ba9e59645873ad87fbc013dcbf1b81d11f33
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-04-01 14:20:28 +0300

    feat: assign multiple values

--- hello.py
+++ hello.py
@@ -27,6 +27,25 @@
 # print(type(y))
 
 # Variable Names
-myvar = "Teo"
-my_var = "Teo"
-myVariableName = "Teo"
+
+# myvar = "Teo"
+# my_var = "Teo"
+# myVariableName = "Teo"
+
+# Assign Multiple Values
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
