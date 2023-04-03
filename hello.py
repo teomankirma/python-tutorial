6f0dc5884b2fbfe19d2af1ef1a776ab3083bed1c
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-04-03 09:20:08 +0300

    feat: for loop

--- hello.py
+++ hello.py
@@ -452,16 +452,25 @@
 
 # While
 
-i = 1
-while i < 6:
-  print(i)
-  if i == 3:
-    break
-  i += 1
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 3:
+#     break
+#   i += 1
 
-i = 1
-while i < 6:
-  print(i)
-  i += 1
-else:
-  print("i is no longer less than 6")
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+
+# For
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in range(6):
+  print(x)
