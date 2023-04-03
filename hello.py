4b3c7dc6f7e11ffc9688bbbc426d947a2f366166
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-04-03 09:42:12 +0300

    feat: lambda

--- hello.py
+++ hello.py
@@ -477,30 +477,41 @@
 
 # Function
 
-def my_function():
-  print("Hello from a function")
+# def my_function():
+#   print("Hello from a function")
 
-my_function()
+# my_function()
 
-def my_function(fname, lname):
-  print(fname + " " + lname)
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
 
-my_function("Emil", "Refsnes")
+# my_function("Emil", "Refsnes")
 
-def my_function(*kids):
-  print("The youngest child is " + kids[2])
+# def my_function(*kids):
+#   print("The youngest child is " + kids[2])
 
-my_function("Emil", "Tobias", "Linus")
+# my_function("Emil", "Tobias", "Linus")
 
-def my_function(**kid):
-  print("His last name is " + kid["lname"])
+# def my_function(**kid):
+#   print("His last name is " + kid["lname"])
 
-my_function(fname = "Tobias", lname = "Refsnes")
+# my_function(fname = "Tobias", lname = "Refsnes")
 
-def my_function(country = "Norway"):
-  print("I am from " + country)
+# def my_function(country = "Norway"):
+#   print("I am from " + country)
 
-my_function("Sweden")
-my_function("India")
-my_function()
-my_function("Brazil")
+# my_function("Sweden")
+# my_function("India")
+# my_function()
+# my_function("Brazil")
+
+# Lambda
+
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
