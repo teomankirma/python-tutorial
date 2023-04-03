06c64b906c72f39cdc83c0c2352138da8d4b0d3a
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-04-03 09:37:00 +0300

    feat: functions

--- hello.py
+++ hello.py
@@ -468,9 +468,39 @@
 
 # For
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
 
-for x in range(6):
-  print(x)
+# for x in range(6):
+#   print(x)
+
+# Function
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
